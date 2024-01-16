27c7790427cb405c339b7a8ce699376e4ae5ee9c
aliangm <risestar0927@outlook.com>
2024-01-17 02:42:32 +0800

    fix: hide retry btn

--- app/ui/Game.tsx
+++ app/ui/Game.tsx
@@ -66,7 +66,7 @@ export default function Game() {
 
         <button
           className={`focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 mt-10 ${
-            characterDead ? 'opacity-1' : 'opacity-0'
+            characterDead && lifes > 0 ? 'opacity-1' : 'opacity-0'
           } transition`}
           onClick={() => setCharacterDead(false)}
         >
